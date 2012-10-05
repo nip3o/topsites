28993595820917b057246fdca5933deb38287fca
Niclas Olofsson <n@niclasolofsson.se>
2012-10-05 11:47:45 +0200

    Added geo info and prettified a bit.

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from lxml import objectify
 from collections import defaultdict
+import math
 
 N = 'niclasolofsson.se'  # used as a mark between requests from different sites
 L = 'Loris.local'  # hostname of my local machine
@@ -21,15 +22,7 @@ class ServerInfo():
     """
     Contains geographic info about a host
     """
-    def __init__(self):
-        self.name = ''
-        self.long = 0.0
-        self.lat = 0.0
-        self.organization = ''
-        self.ip = ''
-        self.country = ''
-
-    def fill(self, name, longitude, lat, org, ip, country):
+    def __init__(self, name, longitude, lat, org, ip, country):
         if org == '-':
             org = name
 
@@ -40,8 +33,20 @@ class ServerInfo():
         self.ip = ip
         self.country = country
 
+    def distance_from(self, lat2, lon2):
+        lat1, lon1 = self.lat, self.long
+        R = 6372.8
+        dLat = math.radians(lat2 - lat1)
+        dLon = math.radians(lon2 - lon1)
+        lat1 = math.radians(lat1)
+        lat2 = math.radians(lat2)
 
-hosts = defaultdict(ServerInfo)
+        a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.sin(dLon / 2) * math.sin(dLon / 2) * math.cos(lat1) * math.cos(lat2)
+        c = 2 * math.asin(math.sqrt(a))
+        return R * c
+
+
+hosts = {}
 
 with open('res.xml') as f:
     xml = objectify.parse(f)
@@ -51,13 +56,14 @@ for r in xml.getroot().getchildren():
     host = unicode(r.host_name)
     # if result was successful
     if hasattr(r, 'geolocation_data'):
+
         # store info for the host
-        hosts[host].fill(name=host,
-                         longitude=float(r.geolocation_data.longitude),
-                         lat=float(r.geolocation_data.latitude),
-                         org=unicode(r.geolocation_data.organization),
-                         ip=unicode(r.resolved_ip_address),
-                         country=unicode(r.geolocation_data.country_name))
+        hosts[host] = ServerInfo(name=host,
+                                 longitude=float(r.geolocation_data.longitude),
+                                 lat=float(r.geolocation_data.latitude),
+                                 org=unicode(r.geolocation_data.organization),
+                                 ip=unicode(r.resolved_ip_address),
+                                 country=unicode(r.geolocation_data.country_name))
 
 sites = []
 
@@ -124,9 +130,17 @@ Total %d contacted hosts. %d unique ip-addresses was contacted. %d unique hostna
 %d sites out of 100 helps google rule the world.""" % (total_hosts, len(real_servers), len(servers),
     len(cheaters), len(helps_google_rule_the_world))
 
+lengths = defaultdict(int)
+ranges = [500, 1000, 5000, 6000, 7000, 8000, 9000, float('inf')]
+for server in servers:
+    for r in ranges:
+        if hosts[server].distance_from(58.3998, 15.577362) < r:
+            lengths[r] += 1
+            break
 
-#for h in hosts:
-    #print "%s,%s" % (hosts[h].lat, hosts[h].long)
+#for l in sorted(lengths):
+#    # output in a LaTeX-friendly style
+#    print "$ < x < %s$ & %s \\\\ \hline" % (l, lengths[l])
 
 #for c in sorted(countries):
 #    print "%s\t&\t%d \\\\ \hline" % (c, len(countries[c]))
